0bda9b58ca6c4737b51b4d41825b025680d9e7d8
Rdinaft <rdinaft@mail.ru>
2023-02-21 12:28:33 +0300

    Fixes added

--- dicts.py
+++ dicts.py
@@ -2,15 +2,13 @@ hero_name = input('Enter your name:')
 
 stats = [
     {
-    'name': hero_name, 
-    'strengh': 10, 
-    'agility': 15, 
-    'intelligence': 2,
-    'talents': ['Shield bash', 'Heroic strike'],
+        'name': hero_name, 
+        'strengh': 10, 
+        'agility': 15, 
+        'intelligence': 2,
+        'talents': ['Shield bash', 'Heroic strike'],
     },
-    
     {'name': 'slime', 'attack': 2, 'health': 10},
-    
     {'name': 'wolf', 'attack': 5, 'health': 14},
 ] 
 stats[0]['talents'].append('Stun')
